63c137a98e3a6939521d60ba9c161a64c99ff7ea
theglitchmaster64 <theglitchmaster64@gmail.com>
2021-06-12 16:49:07 +0530

    init

---
+++ engine/stuff.py
@@ -0,0 +1,8 @@
+def test():
+    print('ok')
+
+class Point:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from engine.stuff import test
+
+if __name__=='__main__':
+    test()
